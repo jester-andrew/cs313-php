a66dfc8272df0a65aafb10aea4c3640191be5a59
alevijester <andrewjester92@gmail.com>
2019-04-29 11:47:17 -0600

    css changes

--- web/index.php
+++ web/index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Home Page</title>
-  <link rel="stylesheet" href="css/homepage.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/homepage.css">
 </head>
+
 <body>
-  <main>
-  <h1>Web Engineering II</h1>
-  <h2>About Me</h2>
-  <p>
-    Hey everyone my name is <strong>Andrew Jester</strong> I am from a small town in western Colorado called Montrose.
-    I have been married for almost 4 years now. My wife and I have a two year old son and a daughter coming in August. 
-    I absolutly love my major of Software enginnering. I work as a TA/Lab Assistant for the cit department over their 
-    web development classes. I work at a company called Avercast in Rigby, ID. We are in the process of building a web 
-    application to replace their legacy system. We are working with Angular for the client side and Java for the server 
-    side of the application. I enjoy being outside hiking and enjpoying the great outdoors the image below is of me and my 
-    son on a hiking though our hikes arent this dangerous most of the time. I look forward to working with you all and learning from you! 
-  </p>
-  <img src="/img/hinking.jpg" alt="Hiking">
-  <h2><a href="/assignments/assignments.html">Assignments Page</a></h2>
-</main>
+    <main>
+        <h1>Web Engineering II</h1>
+        <h2>About Me</h2>
+        <p>
+            Hey everyone my name is <strong>Andrew Jester</strong> I am from a small town in western Colorado called Montrose. I have been married for almost 4 years now. My wife and I have a two year old son and a daughter coming in August. I absolutly
+            love my major of Software enginnering. I work as a TA/Lab Assistant for the cit department over their web development classes. I work at a company called Avercast in Rigby, ID. We are in the process of building a web application to replace
+            their legacy system. We are working with Angular for the client side and Java for the server side of the application. I enjoy being outside hiking and enjpoying the great outdoors the image below is of me and my son on a hiking though our
+            hikes arent this dangerous most of the time. I look forward to working with you all and learning from you!
+        </p>
+        <img src="/img/hinking.jpg" alt="Hiking">
+        <h2><a href="/assignments/assignments.html">Assignments</a></h2>
+    </main>
 
 </body>
+
 </html>
