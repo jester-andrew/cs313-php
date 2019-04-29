55c23fe6ab93363756367fd66f604106d2bd86d3
alevijester <andrewjester92@gmail.com>
2019-04-29 10:53:39 -0600

    start of hw week 2

--- web/index.php
+++ web/index.php
@@ -1,5 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Home Page</title>
+</head>
+<body>
+  <main>
+  <h1>Web Engineering II</h1>
+  <h2>About Me</h2>
+  <p>
+    Hey everyone my name is <strong>Andrew Jester</strong> I am from a small town in western Colorado called Montrose.
+    I have been married for almost 4 years now. My wife and I have a two year old son and a daughter coming in August. 
+    I absolutly love my major of Software enginnering. I work as a TA/Lab Assistant for the cit department over their 
+    web development classes. I work at a company called Avercast in Rigby, ID. We are in the process of building a web 
+    application to replace their legacy system. We are working with Angular for the client side and Java for the server 
+    side of the application. I enjoy being outside hiking and enjpoying the great outdoors the image below is of me and my 
+    son on a hike to a waterfall. I look forward to working with you all and learning from you! 
+  </p>
+  <img src="" alt="Hiking">
+  <h2><a href="/assignments/assignments.php">Assignments Page</a></h2>
+</main>
 
-  phpinfo();
-
-?>
+</body>
+</html>
