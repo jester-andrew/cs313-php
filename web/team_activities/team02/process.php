1d9fddd4bf1c1833a55349b551cc00567b38c818
alevijester <andrewjester92@gmail.com>
2019-05-07 17:29:33 -0600

    css changes

--- web/team_activities/team02/process.php
+++ web/team_activities/team02/process.php
@@ -1,7 +1,23 @@
 <?php
-$name = $_GET['name'];
-$email = $_GET['email'];
-$major = $_GET['major'];
-$commnt = $_GET['comments'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$commnt = $_POST['comments'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student</title>
+</head>
+<body>
+    <h1>Welcome <?php echo $name; ?></h1>
+    <a href="mailto:<?php echo $email; ?>"></a>
+    <p>Major: <?php echo $major; ?></p>
+    <p>Comments: <?php echo $commnt; ?></p>
+    
+</body>
+</html>
 
-echo "$name, $email, $major, $commnt";
