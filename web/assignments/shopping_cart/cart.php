abcb6bd8f51aba99d25a6e7e69af8e877d412a5c
alevijester <andrewjester92@gmail.com>
2019-05-06 21:36:29 -0600

    week03 initial

--- web/assignments/shopping_cart/cart.php
+++ web/assignments/shopping_cart/cart.php
@@ -13,13 +13,10 @@ session_start();
     <?php 
     //var_dump($_SESSION['cart']); 
     $table = "<table><tr><th>Item Name</th><th>Qty.</th><th>Price</th></tr>";
-    $items = $_SESSION['cart'];
-
+    $items = json_decode($_SESSION['cart'], true);
+    var_dump($items);
     foreach($items as $item){
-        $obj = json_decode($item, true);
-        
-        //var_dump($obj);
-        $table .= '<tr><td>'. $obj['name'] .'</td><td>'. $obj['quantity'] .'</td><td>'. $obj['Price'] .'</td></tr>';
+        $table .= '<tr><td>'. $item['name'] .'</td><td>'. $item['quantity'] .'</td><td>'. $item['Price'] .'</td></tr>';
       }
 
     $table .= "</table>";
