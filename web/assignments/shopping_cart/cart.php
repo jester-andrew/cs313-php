5877a93b82ce2410d9a519d6925ac1d355a473ff
alevijester <andrewjester92@gmail.com>
2019-05-07 11:26:04 -0600

    css changes

--- web/assignments/shopping_cart/cart.php
+++ web/assignments/shopping_cart/cart.php
@@ -7,9 +7,11 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
     <title>Shopping cart</title>
 </head>
 <body>
+    <h1>Shopping Cart</h1>
     <?php  
     $table = "<table><tr><th>Item Name</th><th>Qty.</th><th>Price</th></tr>";
     $items = $_SESSION['cart'];
