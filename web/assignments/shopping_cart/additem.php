70c945c9cacf94fd1af5c805bb9e4ca6beeac0b1
alevijester <andrewjester92@gmail.com>
2019-05-06 20:10:12 -0600

    week03 initial

--- web/assignments/shopping_cart/additem.php
+++ web/assignments/shopping_cart/additem.php
@@ -6,6 +6,6 @@ $data = filter_input(INPUT_GET, 'data');
 if( isset($_SESSION['cart']) ){
     array_push($_SESSION['cart'], $data);
 }else{
-    $_SESSION['cart'] = array(data);
+    $_SESSION['cart'] = array($data);
 }
 
