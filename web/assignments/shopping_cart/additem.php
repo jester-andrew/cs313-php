411ed6362505c9e8820bed168baa19f22de8530c
alevijester <andrewjester92@gmail.com>
2019-05-08 11:57:20 -0600

    css

--- web/assignments/shopping_cart/additem.php
+++ web/assignments/shopping_cart/additem.php
@@ -5,6 +5,7 @@ $data = filter_input(INPUT_GET, 'data');
 
 if( isset($_SESSION['cart']) ){
     array_push($_SESSION['cart'], $data);
+    sort($_SESSION['cart']);
 }else{
     $_SESSION['cart'] = array($data);
 }
