0d0a1142c56c5984181f47d07bb21d68db8e0dda
alevijester <andrewjester92@gmail.com>
2019-05-20 10:59:54 -0600

    initial mountains

--- web/assignments/mountains/functions.php
+++ web/assignments/mountains/functions.php
@@ -3,7 +3,7 @@
 function createNavigation($ranges){
     $nav = "<ul>";
     foreach($ranges as $range){
-        $nav .= '<li><a id="'.$range['ID'].'">'.$range['RangeName'].'</a></li>';
+        $nav .= '<li><a href=index.php/?action=range&id='.$range['ID'].'>'.$range['RangeName'].'</a></li>';
     }
     $nav .= "</ul>";
 
