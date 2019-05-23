7332e9811f2de5b0271f1bc07d94fe577953794e
alevijester <andrewjester92@gmail.com>
2019-05-23 07:47:41 -0600

    css and functionality update

--- web/assignments/mountains/views/sign-up.php
+++ web/assignments/mountains/views/sign-up.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/assignments/mountains/css/style.css">
-    <title>Document</title>
+    <title>
+    <?php 
+        if($_SESSION['ranges']){
+            echo $_SESSION['ranges'][$rangeID - 1]['RangeName'] . "| Colorado's 14ers";
+        }
+        ?>
+    </title>
 </head>
 <body>
 <div id="wrapper">
-    <header>
-        <h1>Colorado's 14ers</h1>
+<header>
+<h1>Colorado's 14ers</h1>
         <nav>
             <?php
             if(isset($nav)){
@@ -20,13 +26,12 @@
             <a href="/assignments/mountains/index.php?action=sign-in">Sign-in</a>
         </nav>
     </header>
-    <main>
-    Sign-in
-    <a href="/assignments/mountains/index.php?action=sign-up">Sign-up</a>
+    <main id="range">
+        sign-up
     </main>
     <footer>
         <p>Last Ppdated 5/22/2019</p>
     </footer>
-    <div id="wrapper">
+    </div>
 </body>
 </html>
