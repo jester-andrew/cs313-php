d32c5af05352bca865568bbd78125891f1d9ca83
alevijester <andrewjester92@gmail.com>
2019-05-23 16:12:53 -0600

    css and functionality update

--- web/assignments/mountains/views/home.php
+++ web/assignments/mountains/views/home.php
@@ -18,11 +18,11 @@
             }
             ?>
             <a href="/assignments/mountains/index.php?action=sign-in">Sign-in</a>
-            <p>(Image from 14ers.com)</p>
         </nav>
     </header>
     <main id="home">
     <img src="/assignments/mountains/img/mt.-sneffles.jpg" alt="Sneffles" id="homeimg">
+    <p>(Image from 14ers.com)</p>
     <p>Welcome to colorado's 14ers. We strive to give you simple facts about the 14ers
      in Colorado and point you where to go if you want to learn more or go for a hike! Enjoy
     this picture of Mt. Sneffles.</p>
