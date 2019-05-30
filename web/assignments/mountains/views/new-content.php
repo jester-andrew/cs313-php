eac5b0e49f1cb815739f158b87cfe2113f0593ef
alevijester <andrewjester92@gmail.com>
2019-05-29 20:13:31 -0600

    login

--- web/assignments/mountains/views/new-content.php
+++ web/assignments/mountains/views/new-content.php
@@ -35,10 +35,10 @@
         ?>
         <br>
         <input type="text" name="peak-name" id="peak" placeholder="Peak Name"><br>
-        <input type="text" name="elevation" id="elevation" placeholder="Elevation in feet"><br>
+        <input type="text" name="elevation" id="elevation" placeholder="Elevation ex: 14000"><br>
         <input type="text" name="class" id="class" placeholder="class: 1-5"><br>
         <input type="text" name="link" id="link" placeholder="14ers.com link"><br>
-        <input type="text" name="img-path" id="path" value="/assignments/mountains/img/imageName.jpg"><br>
+        <input type="text" name="img-path" id="path" value="/assignments/mountains/img/IMAGE-NAME-HERE.jpg"><br>
         <textarea name="info" id="info" cols="30" rows="10">Mountain peak info here</textarea><br>
 
         <input type="submit" value="Add Peak">
